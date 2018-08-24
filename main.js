2dd27f251bfb9eae72993f386f17b0cdfc2c67de
Brendan Woodell <brendan.woodell@gmail.com>
2018-08-23 20:15:59 -0700

    moving div Hero around, added walls

--- main.js
+++ main.js
@@ -1,33 +1,107 @@
 document.addEventListener('DOMContentLoaded', function(e) {
 
 let table = document.querySelector("tbody")
+let currlvl = maps[0];
+let position = []
 
-for(let i in map1){
-  let tr = document.createElement("tr")
-  table.appendChild(tr)
-  for(let j in map1[i]) {
-    let td = document.createElement("td")
-    table.appendChild(td)
-    if(map1[i][j] === 1)
-    {
-      td.id= 'user'
+function render(map){
+
+  for(let i in map){
+    let tr = document.createElement("tr")
+    tr.id = i
+    table.appendChild(tr)
+    for(let j in map[i]) {
+      let td = document.createElement("td")
+      td.className = j
+      tr.appendChild(td)
+      if(map[i][j] === 1)
+      {
+        let usr = document.createElement("div")
+        usr.id = "user"
+        td.appendChild(usr)
+        position[0] = parseInt(i)
+        position[1] = parseInt(j)
+      }
+      else if(map[i][j] === 2)
+      {
+        td.classList.add("wall")
+      }
     }
   }
 }
 
+render(currlvl)
+
+console.log(currlvl[0][0])
 
 function move(direction) {
   //find location of current id=user
   //move the id in that direction
   //re-render
+  console.log(currlvl)
+  const checkloc = (x,y,matrix) => {
+    if(matrix[x] !== undefined) {
+      if(matrix[x][y] !== undefined) {
+        return true
+      }
+    }
+  }
 
+  let user = document.getElementById("user")
+
+  switch (direction) {
+    case "ArrowUp":
+    console.log(position)
+      if(checkloc(position[0] - 1, position[1], currlvl)){
+          let newtd = document.getElementById(`${position[0]-1}`).getElementsByClassName(`${position[1]}`)[0]
+            if(newtd.classList.contains("wall") === false)
+            {
+              newtd.append(user)
+              position[0]--
+            }
+      }
+      break;
+    case "ArrowDown":
+    if(checkloc(position[0] + 1, position[1], currlvl)){
+        let newtd = document.getElementById(`${position[0]+1}`).getElementsByClassName(`${position[1]}`)[0]
+        if(newtd.classList.contains("wall") === false)
+        {
+          newtd.append(user)
+          position[0]++
+        }
+
+    }
+    break;
+    case "ArrowLeft":
+    if(checkloc(position[0], position[1] -1, currlvl)){
+        let newtd = document.getElementById(`${position[0]}`).getElementsByClassName(`${position[1]-1}`)[0]
+        if(newtd.classList.contains("wall") === false)
+        {
+          newtd.append(user)
+          position[1]--
+        }
+    }
+    break;
+    case "ArrowRight":
+    if(checkloc(position[0], position[1] + 1, currlvl)){
+        let newtd = document.getElementById(`${position[0]}`).getElementsByClassName(`${position[1]+1}`)[0]
+        if(newtd.classList.contains("wall") === false)
+        {
+          newtd.append(user)
+          position[1]++
+        }
+    }
+    break;
+  }
 }
+
 
 document.addEventListener('keydown', function(e){
   e.preventDefault()
 
   //console.log(e.key)
   //ArrowUp, ArrowDown, ArrowRight, ArrowLeft
+  move(e.key)
 
 })
 
